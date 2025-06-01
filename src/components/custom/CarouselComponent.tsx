8d0f1ae64b572eca4b1f21ba9363f7dc9ca2e161
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-06-01 19:31:46 +0600

    fixed the image height

--- src/components/custom/CarouselComponent.tsx
+++ src/components/custom/CarouselComponent.tsx
@@ -9,42 +9,42 @@ import Autoplay from 'embla-carousel-autoplay';
 
 const CarouselComponent = () => {
   return (
-    <div className="px-36 my-28">
+    <div className="px-36 my-28 h-full">
       <h1 className="text-xl py-5 text-center">Gallery</h1>
-      <div className="max-w-md mx-auto">
+      <div className="max-w-md mx-auto h-full">
         <Carousel
-          className=""
+          className="h-full"
           plugins={[
             Autoplay({
               delay: 2000,
             }),
           ]}
         >
-          <CarouselContent className="h-svh">
-            <CarouselItem className="w-[20px] h-[20px]">
+          <CarouselContent className="">
+            <CarouselItem className="w-full h-full">
               <img
                 src="https://wallpaperswide.com/download/green_nature-wallpaper-1600x1200.jpg"
                 alt=""
                 loading="lazy"
-                className="w-full h-svh rounded-md"
+                className="w-full h-full rounded-xl"
               />
             </CarouselItem>
-            <CarouselItem className="w-[20px] h-[20px]">
+            <CarouselItem className="w-full h-full">
               {' '}
               <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9VdqXP-OFKmFFUscr10_51gtyLPYvlJREtg&s"
                 alt=""
                 loading="lazy"
-                className="w-full h-svh rounded-md"
+                className="w-full h-full rounded-xl"
               />
             </CarouselItem>
-            <CarouselItem className="w-[20px] h-[20px]">
+            <CarouselItem className="w-full h-full">
               {' '}
               <img
                 src="https://res.cloudinary.com/dtljonz0f/image/upload/c_auto,ar_4:3,w_3840,g_auto/f_auto/q_auto/v1/shutterstock_2475547453_1_arkhzn?_a=BAVAZGE70"
                 alt=""
                 loading="lazy"
-                className="w-full h-svh rounded-md"
+                className="w-full h-full rounded-xl"
               />
             </CarouselItem>
           </CarouselContent>
