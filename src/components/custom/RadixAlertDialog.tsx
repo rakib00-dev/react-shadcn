7ae7d1b3fb3a4eee7104abdc3bc9c8dd7bc533b0
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-06-01 20:58:11 +0600

    added RadixAlertDialog

---
+++ src/components/custom/RadixAlertDialog.tsx
@@ -0,0 +1,16 @@
+import { AlertDialog } from 'radix-ui';
+
+export default () => (
+  <AlertDialog.Root>
+    <AlertDialog.Trigger />
+    <AlertDialog.Portal>
+      <AlertDialog.Overlay />
+      <AlertDialog.Content>
+        <AlertDialog.Title />
+        <AlertDialog.Description />
+        <AlertDialog.Cancel />
+        <AlertDialog.Action />
+      </AlertDialog.Content>
+    </AlertDialog.Portal>
+  </AlertDialog.Root>
+);
