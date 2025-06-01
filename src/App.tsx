81b5f6f7bda6e63ac3abd59bb3720b1b99da7794
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-06-01 19:23:04 +0600

    added embla-carousel-autoplay and implemented to the carouselComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import Accordion from './components/custom/Accordion';
 import CompanyCarousel from './components/custom/Carousel';
+import CarouselComponent from './components/custom/CarouselComponent';
 import Hero from './components/custom/Hero';
 import Navbar from './components/custom/Navbar';
 
 function App() {
   return (
-    <>
+    <div className="overflow-hidden">
       <Navbar />
       <Hero />
       <CompanyCarousel />
       <Accordion />
-    </>
+      <CarouselComponent />
+    </div>
   );
 }
 
---
+++ src/components/custom/CarouselComponent.tsx
@@ -0,0 +1,59 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import Autoplay from 'embla-carousel-autoplay';
+
+const CarouselComponent = () => {
+  return (
+    <div className="px-36 my-28">
+      <h1 className="text-xl py-5 text-center">Gallery</h1>
+      <div className="max-w-md mx-auto">
+        <Carousel
+          className=""
+          plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
+        >
+          <CarouselContent className="h-svh">
+            <CarouselItem className="w-[20px] h-[20px]">
+              <img
+                src="https://wallpaperswide.com/download/green_nature-wallpaper-1600x1200.jpg"
+                alt=""
+                loading="lazy"
+                className="w-full h-svh"
+              />
+            </CarouselItem>
+            <CarouselItem className="w-[20px] h-[20px]">
+              {' '}
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9VdqXP-OFKmFFUscr10_51gtyLPYvlJREtg&s"
+                alt=""
+                loading="lazy"
+                className="w-full h-svh"
+              />
+            </CarouselItem>
+            <CarouselItem className="w-[20px] h-[20px]">
+              {' '}
+              <img
+                src="https://res.cloudinary.com/dtljonz0f/image/upload/c_auto,ar_4:3,w_3840,g_auto/f_auto/q_auto/v1/shutterstock_2475547453_1_arkhzn?_a=BAVAZGE70"
+                alt=""
+                loading="lazy"
+                className="w-full h-svh"
+              />
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default CarouselComponent;
